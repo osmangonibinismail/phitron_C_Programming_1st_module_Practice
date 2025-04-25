ca926e3fcd4af22e6b7051614d8b60690ac94638
Osmangonibinismail <mdosmangoni4314@gmail.com>
2025-04-25 12:01:03 +0600

    first commit

---
+++ code.c
@@ -0,0 +1,72 @@
+// #include<stdio.h>
+// int main()
+// {
+//     printf("hello world\n");
+//     printf("Hi I am\tOsman Goni");
+
+//     return 0;
+// } 
+
+// #include<stdio.h>
+// int main()
+// {
+//     int num1;
+//     num1 = 50;
+//     float f = 3.1416;
+//     char c = '@';
+//     printf("%d\n", num1);
+//     printf("%.4f\n", f);
+//     printf("%c", c);
+// }
+
+// #include<stdio.h>
+// #include<stdbool.h>
+
+// int main()
+// {
+//     bool b;
+//     bool c;
+//     b = true;
+//     c = false;
+
+//     printf("%d\n", b);
+//     printf("%d\n", c);
+
+//     return 0;
+// }
+
+// #include<stdio.h>
+
+// int main()
+// {
+//     int a = 10000000000;
+//     printf("%d", a);
+
+//     return 0;
+// }
+// #include<stdio.h>
+
+// int main()
+// {
+//     long long int a = 1000000000000;
+//     printf("%lld", a);
+
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     float f = 222.444;
+//     printf("%f", f);
+
+//     return 0;
+// }
+#include<stdio.h>
+int main()
+{
+    double f = 222.448565;
+    printf("%lf", f);
+
+    return 0;
+}
---
+++ input.c
@@ -0,0 +1,50 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+
+//     scanf("%d", &a);
+//     printf("%d", a);
+
+//     return 0;
+// }
+// #include<stdio.h>
+// #include<stdbool.h>
+// int main()
+// {
+//     int a;
+//     float f;
+//     char c;
+
+//     scanf("%d\n", &a);
+//     printf("%d\n", a);
+//     scanf("%f\n", &f);
+//     printf("%.1f\n", f);
+//     scanf("%c\n", &c);
+//     printf("%c\n", c);
+
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     float f;
+//     char c;
+
+//     scanf("%d %f %c", &a, &f, &c);
+//     printf("%d %.2f %c", a, f, c);
+// }
+
+#include<stdio.h>
+
+int main()
+{
+    long long int a;
+
+    scanf("%lld", &a);
+    printf("%lld", a);
+
+    return 0;
+}
